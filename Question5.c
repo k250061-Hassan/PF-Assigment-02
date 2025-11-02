0bde44db1df4de567464813f43b85efc3aa7f4f5
Hassan Ali Khan <k250061@nu.edu.pk>
2025-11-02 14:27:22 +0500

    Add files via upload

--- Question5.c
+++ Question5.c
@@ -1,96 +1,93 @@
+  
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX_LEN 100
 
-void encodeMessage(char message[]){
+void encodeMessage(char message[])
+{
+    // string reversal
+    int len = strlen(message);
+    char reversedMessage[len + 1];
 
-        int reverse[MAX_LEN];
-        int s = strlen(message);
-        int temp;
 
-    temp = message[1];
-    message[1] = message[4];
-    message[4] = temp;        
-
-    printf("---In letters---: \n");
-    for(int i=s-1, j=0; i>=0; i--, j++){
-        reverse[j] = message[i];
-        printf("%c", reverse[j]);
+    for (int i = 0; i < len; i++)
+    {
+        reversedMessage[i] = message[len - 1 - i];
     }
-    printf("\n---Binary Code---\n");
-    for(int i=0; i<s; i++){
-        for(int j=7; j>=0; j--){
-            printf("%d", (reverse[i] >> j) & 1);
-        }
+    reversedMessage[len] = '\0';
+
+    // toggling the 2nd & 5th bit of the reversedMessage
+    // toggling the 2nd and 5th bits means: XOR 18 (16 + 2)
+    char encodedMessage[len + 1];
+    for (int i = 0; i < len; i++)
+    {
+        encodedMessage[i] = (char) (reversedMessage[i] ^ 18);     // XOR 18
     }
-    
+    encodedMessage[len] = '\0';
+
+    printf("  > Original: %s\n  > Reversed: %s\n  > Encoded: %s", message, reversedMessage, encodedMessage);
+}
+void decodeMessage(char message[])
+{
+    // untoggling the 2nd & 5th bit of the message
+    int len = strlen(message);
+    char decodedMessage[len + 1];
+    for (int i = 0; i < len; i++)
+    {
+        decodedMessage[i] = (char) (message[i] ^ 18);
+    }
+    decodedMessage[len] = '\0';
+
+    // string reversal of the decodedMessage
+    char reversedMessage[len + 1];
+
+
+    for (int i = 0; i < len; i++)
+    {
+        reversedMessage[i] = decodedMessage[len - 1 - i];
+    }
+    reversedMessage[len] = '\0';  
+
+    printf("  > Original: %s\n  > Decoded: %s\n  > Reversed: %s", message, decodedMessage, reversedMessage);
 }
 
-    void decodeMessage(char message[]){
-        int reverse[MAX_LEN];
-        int s = strlen(message);
-        int temp;
 
-            temp = message[1];
-            message[1] = message[4];
-            message[4] = temp;        
-            
-    printf("---In letters---: \n");
-    for(int i=s-1, j=0; i>=0; i--, j++){
-        reverse[j] = message[i];
-        printf("%c", reverse[j]);
-    }
-}
 
-void showMenu(void) {
-    printf("\n============================================\n");
-      printf("     TCS Message Encoding/Decoding Utility  \n");
-      printf("============================================\n");
-    printf("1. Encode Message\n");
-    printf("2. Decode Message\n");
-    printf("3. EXIT\n");
-    printf("--------------------------------------------\n");
-}
 
-int main(){
-
-    int choice;
-    char message[MAX_LEN];
-
-    do{
-        showMenu();
-
-        printf("Enter your choice: ");
-        if(scanf("%d", &choice) != 1){
-            printf("\nInvalid Input. Please enter a number\n");
-            while(getchar() != '\n');
-            continue;
-        } while(getchar() != '\n');
-
-        switch(choice){
-
-            case 1: 
-            printf("Enter message to Encode: ");
-            scanf(" %[^\n]", message);
-            while(getchar() != '\n');
-            encodeMessage(message);
-            break;
-
-            case 2:
-            printf("Enter message to Decode: ");
-            scanf(" %[^\n]", message);
-            while(getchar() != '\n');
-            decodeMessage(message);
-
-            case 3:
-            printf("Exiting the System. Goodbye!");break;
-
-            default: printf("Invalid option. Please enter between 1-3.");break;
-  
+int main()
+{
+    while (true)
+    {
+        system("cls");      // clearing the terminal screen
+        printf("===========================\n| Message Encoder/Decoder |\n===========================\n\n\n[Main Menu]\n  [1] Encode Message\n  [2] Decode Message\n  [3] Exit");
+       
+        char userInput[63];       
+        while (true)
+        {
+            printf("\nEnter an option [1-3]: ");
+            fgets(userInput, sizeof(userInput), stdin);
+            if   ((strlen(userInput) == 2) && (userInput[0] == '1' || userInput[0] == '2' || userInput[0] == '3')){ break; }
+            else                                                                                                  { printf("> [!] Please enter 1,2, or 3"); }
         }
 
-    }while(choice != 3);
 
-    return 0;
+        if (userInput[0] == '3'){ return 0; }         
+        else                                          
+        {
+            printf("\n\n\n[%s Message]\nEnter the message to %s: ",
+                    (userInput[0] == '1')? "Encode" : "Decode", (userInput[0] == '1')? "encode" : "decode");
+            char userMessage[63];
+            fgets(userMessage, sizeof(userMessage), stdin);
+            userMessage[strlen(userMessage) - 1] = '\0';      
+
+            if   (userInput[0] == '1'){ encodeMessage(userMessage); }
+            else                      { decodeMessage(userMessage); }
+
+            printf("\n\n\n\n\nPress enter to continue: ");
+            fgets(userInput, sizeof(userInput), stdin);    
+        
+        }
+    }
 }
